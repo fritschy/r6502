9249d094221fb60d9a066acc6288b8e382219618
Marcus Borkenhagen <m@fritschy.de>
2021-02-17 23:27:56 +0100

    adressing mode of instructions
    Branches need to know how many bytes its operand is long in order to eat
    those bytes even if the branch is not taken. This was not realized at
    all before.
    
    Instruction implementations now take a adressing_mode::AM value instead
    of the addressing mode implementation function. The "AM" can return its
    implementation function using AM::dispatch() and its operand length with
    AM::length().
    
    It would still be sensible to put all this info into tables - but that's
    all I got for today, go away now!

--- sixfiveohtwo/src/adressing_mode.rs
+++ sixfiveohtwo/src/adressing_mode.rs
@@ -1,21 +1,27 @@
 use crate::{Memory, R6502};
 
+/// A value from an addressing mode, can either be the value that was
+/// read from memory, or only its address, in which case, no memory access
+/// is done (i.e. perhaps you need the address for writing to it)
 pub enum AMValue {
     Address(u16),
     Value(u8),
+    /// Sometimes we need to differentiate between a value in memory or
+    /// an accumulator, as certain instructions can work on values in memory
+    /// or the accumulator.
     Accumulator,
 }
 
 impl AMValue {
-    pub fn to_addr(self) -> u16 {
-        match self {
+    pub fn to_addr(&self) -> u16 {
+        match *self {
             AMValue::Address(a) => a,
             _ => panic!("Cannot get address of a Value"),
         }
     }
 
-    pub fn to_value(self) -> u8 {
-        match self {
+    pub fn to_value(&self) -> u8 {
+        match *self {
             AMValue::Value(a) => a,
             _ => panic!("Cannot get value of an Address"),
         }
@@ -36,29 +42,91 @@ impl From<u16> for AMValue {
 
 #[derive(Eq, PartialEq, Copy, Clone)]
 pub enum AMSelect {
+    /// want to compute the address only (no memory read, useful for writing to memory)
     A,
+    /// want to read the value from memory
     V,
 }
 
-pub fn implied<M: Memory>(_cpu: &mut R6502<M>, _mode: AMSelect) -> AMValue {
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub enum AM {
+    /// implied
+    IMPL,
+    /// immediate
+    IM,
+    /// accumulator
+    A,
+    /// absolute
+    ABS,
+    /// x indexed absolute
+    XIA,
+    /// y indexed absolute
+    YIA,
+    /// absolute indirect
+    AI,
+    /// zero page
+    Z,
+    /// x indexed zero page
+    XIZ,
+    /// y indexed zero page
+    YIZ,
+    /// x indirexed zero page indirect
+    XIZI,
+    /// zero page indirect y indexed
+    ZIYI,
+    /// relative
+    REL,
+}
+
+impl AM {
+    /// return length of operand in bytes
+    pub fn length(self) -> u16 {
+        match self {
+            AM::IMPL | AM::A => 0,
+            AM::ABS | AM::AI | AM::XIA | AM::YIA => 2,
+            _ => 1,
+        }
+    }
+
+    /// return addressing mode implementation function
+    pub fn dispatch<M: Memory>(self) -> impl Fn(&mut R6502<M>, AMSelect) -> AMValue {
+        match self {
+            AM::IMPL => implied,
+            AM::IM   => immediate,
+            AM::A    => accumulator,
+            AM::ABS  => absolute,
+            AM::XIA  => x_indexed_absolute,
+            AM::YIA  => y_indexed_absolute,
+            AM::AI   => absolute_indirect,
+            AM::Z    => zero_page,
+            AM::XIZ  => x_indexed_zero_page,
+            AM::YIZ  => y_indexed_zero_page,
+            AM::XIZI => x_indexed_zero_page_indirect,
+            AM::ZIYI => zero_page_indirect_y_indexed,
+            AM::REL  => relative,
+        }
+    }
+}
+
+fn implied<M: Memory>(_cpu: &mut R6502<M>, _mode: AMSelect) -> AMValue {
     unreachable!("implied addressing mode should NEVER try to resolve its operand");
 }
 
-pub fn immediate<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
+fn immediate<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
     match mode {
         AMSelect::A => cpu.r.pc.into(),
         AMSelect::V => cpu.fetch_byte_with_pc().into(),
     }
 }
 
-pub fn accumulator<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
+fn accumulator<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
     match mode {
         AMSelect::V => cpu.r.a.into(),
         AMSelect::A => AMValue::Accumulator,
     }
 }
 
-pub fn absolute<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
+fn absolute<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
     let addr = cpu.fetch_byte_with_pc() as u16;
     let addr = addr | (cpu.fetch_byte_with_pc() as u16) << 8;
     match mode {
@@ -67,7 +135,7 @@ pub fn absolute<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
     }
 }
 
-pub fn x_indexed_absolute<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
+fn x_indexed_absolute<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
     let addr = absolute(cpu, AMSelect::A).to_addr();
     let addr = addr.wrapping_add(cpu.r.x as u16);
     match mode {
@@ -76,7 +144,7 @@ pub fn x_indexed_absolute<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMVa
     }
 }
 
-pub fn y_indexed_absolute<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
+fn y_indexed_absolute<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
     let addr = absolute(cpu, AMSelect::A).to_addr();
     let addr = addr.wrapping_add(cpu.r.y as u16);
     match mode {
@@ -86,7 +154,7 @@ pub fn y_indexed_absolute<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMVa
 }
 
 // This is only ever possible with JMP, hence we just set the PC
-pub fn absolute_indirect<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
+fn absolute_indirect<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
     let addr = absolute(cpu, AMSelect::A).to_addr();
     let next = cpu.read_byte(addr) as u16;
     let next = next | (cpu.read_byte(addr.wrapping_add(1)) as u16) << 8;
@@ -96,7 +164,7 @@ pub fn absolute_indirect<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMVal
     }
 }
 
-pub fn zero_page<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
+fn zero_page<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
     let op = cpu.fetch_byte_with_pc() as u16;
     match mode {
         AMSelect::A => op.into(),
@@ -104,7 +172,7 @@ pub fn zero_page<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
     }
 }
 
-pub fn x_indexed_zero_page<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
+fn x_indexed_zero_page<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
     let op = cpu.fetch_byte_with_pc();
     let addr = cpu.r.x.wrapping_add(op) as u16;
     match mode {
@@ -113,7 +181,7 @@ pub fn x_indexed_zero_page<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMV
     }
 }
 
-pub fn y_indexed_zero_page<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
+fn y_indexed_zero_page<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
     let op = cpu.fetch_byte_with_pc();
     let addr = cpu.r.y.wrapping_add(op) as u16;
     match mode {
@@ -122,7 +190,7 @@ pub fn y_indexed_zero_page<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMV
     }
 }
 
-pub fn x_indexed_zero_page_indirect<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
+fn x_indexed_zero_page_indirect<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
     let op = cpu.fetch_byte_with_pc();
     let addr = cpu.r.x.wrapping_add(op) as u16;
     let next = cpu.read_byte(addr) as u16;
@@ -133,7 +201,7 @@ pub fn x_indexed_zero_page_indirect<M: Memory>(cpu: &mut R6502<M>, mode: AMSelec
     }
 }
 
-pub fn zero_page_indirect_y_indexed<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
+fn zero_page_indirect_y_indexed<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
     let op = cpu.fetch_byte_with_pc();
     let addr = op as u16;
     let next = cpu.read_byte(addr) as u16;
@@ -146,7 +214,7 @@ pub fn zero_page_indirect_y_indexed<M: Memory>(cpu: &mut R6502<M>, mode: AMSelec
 }
 
 // XXX: this is only ever used by JMP
-pub fn relative<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
+fn relative<M: Memory>(cpu: &mut R6502<M>, mode: AMSelect) -> AMValue {
     let op = cpu.fetch_byte_with_pc();
     let op = op as i8;
     // XXX I am not happy with this wild around sign/unsign casting
--- sixfiveohtwo/src/instr.rs
+++ sixfiveohtwo/src/instr.rs
@@ -1,63 +1,63 @@
 use crate::{status_flag, Memory, R6502};
 
-use crate::adressing_mode::{AMSelect, AMValue};
+use crate::adressing_mode::{AMSelect, AMValue, AM};
 
 use std::ops::{BitAnd, BitOr, BitXor};
 
 pub fn lda<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    cpu.r.a = addr_mode(cpu, AMSelect::V).to_value();
+    cpu.r.a = addr_mode.dispatch()(cpu, AMSelect::V).to_value();
     cpu.set_flag(status_flag::N, cpu.r.a & 0x80 != 0);
     cpu.set_flag(status_flag::Z, cpu.r.a == 0);
 }
 
 pub fn ldx<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    cpu.r.x = addr_mode(cpu, AMSelect::V).to_value();
+    cpu.r.x = addr_mode.dispatch()(cpu, AMSelect::V).to_value();
     cpu.set_flag(status_flag::N, cpu.r.x & 0x80 != 0);
     cpu.set_flag(status_flag::Z, cpu.r.x == 0);
 }
 
 pub fn ldy<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    cpu.r.y = addr_mode(cpu, AMSelect::V).to_value();
+    cpu.r.y = addr_mode.dispatch()(cpu, AMSelect::V).to_value();
     cpu.set_flag(status_flag::N, cpu.r.y & 0x80 != 0);
     cpu.set_flag(status_flag::Z, cpu.r.y == 0);
 }
 
 pub fn sta<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let addr = addr_mode(cpu, AMSelect::A).to_addr();
+    let addr = addr_mode.dispatch()(cpu, AMSelect::A).to_addr();
     cpu.write_byte(addr, cpu.r.a);
 }
 
 pub fn stx<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let addr = addr_mode(cpu, AMSelect::A).to_addr();
+    let addr = addr_mode.dispatch()(cpu, AMSelect::A).to_addr();
     cpu.write_byte(addr, cpu.r.x);
 }
 
 pub fn sty<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let addr = addr_mode(cpu, AMSelect::A).to_addr();
+    let addr = addr_mode.dispatch()(cpu, AMSelect::A).to_addr();
     cpu.write_byte(addr, cpu.r.y);
 }
 
 pub fn tax<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.r.x = cpu.r.a;
     cpu.set_flag(status_flag::N, cpu.r.x & 0x80 != 0);
@@ -66,7 +66,7 @@ pub fn tax<M: Memory>(
 
 pub fn tay<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.r.y = cpu.r.a;
     cpu.set_flag(status_flag::N, cpu.r.y & 0x80 != 0);
@@ -75,7 +75,7 @@ pub fn tay<M: Memory>(
 
 pub fn tsx<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.r.x = cpu.r.sp;
     cpu.set_flag(status_flag::N, cpu.r.x & 0x80 != 0);
@@ -84,7 +84,7 @@ pub fn tsx<M: Memory>(
 
 pub fn txa<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.r.a = cpu.r.x;
     cpu.set_flag(status_flag::N, cpu.r.a & 0x80 != 0);
@@ -93,14 +93,14 @@ pub fn txa<M: Memory>(
 
 pub fn txs<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.r.sp = cpu.r.x;
 }
 
 pub fn tya<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.r.a = cpu.r.y;
     cpu.set_flag(status_flag::N, cpu.r.a & 0x80 != 0);
@@ -109,21 +109,21 @@ pub fn tya<M: Memory>(
 
 pub fn pha<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.push(cpu.r.a);
 }
 
 pub fn php<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.push(cpu.r.sr);
 }
 
 pub fn pla<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.r.a = cpu.pop();
     cpu.set_flag(status_flag::N, cpu.r.a & 0x80 != 0);
@@ -132,16 +132,16 @@ pub fn pla<M: Memory>(
 
 pub fn plp<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.r.sr = cpu.pop();
 }
 
 pub fn asl<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let m = addr_mode(cpu, AMSelect::A);
+    let m = addr_mode.dispatch()(cpu, AMSelect::A);
     let (o, m) = match m {
         AMValue::Accumulator => {
             let a = cpu.r.a;
@@ -164,9 +164,9 @@ pub fn asl<M: Memory>(
 
 pub fn lsr<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let m = addr_mode(cpu, AMSelect::A);
+    let m = addr_mode.dispatch()(cpu, AMSelect::A);
     let (o, m) = match m {
         AMValue::Accumulator => {
             let a = cpu.r.a;
@@ -189,9 +189,9 @@ pub fn lsr<M: Memory>(
 
 pub fn rol<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let m = addr_mode(cpu, AMSelect::A);
+    let m = addr_mode.dispatch()(cpu, AMSelect::A);
     let (o, m) = match m {
         AMValue::Accumulator => {
             let a = cpu.r.a;
@@ -219,9 +219,9 @@ pub fn rol<M: Memory>(
 
 pub fn ror<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let m = addr_mode(cpu, AMSelect::A);
+    let m = addr_mode.dispatch()(cpu, AMSelect::A);
     let (o, m) = match m {
         AMValue::Accumulator => {
             let a = cpu.r.a;
@@ -249,9 +249,9 @@ pub fn ror<M: Memory>(
 
 pub fn and<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let m = addr_mode(cpu, AMSelect::V).to_value();
+    let m = addr_mode.dispatch()(cpu, AMSelect::V).to_value();
     cpu.r.a = cpu.r.a.bitand(m);
     cpu.set_flag(status_flag::N, cpu.r.a & 0x80 != 0);
     cpu.set_flag(status_flag::Z, cpu.r.a == 0);
@@ -259,9 +259,9 @@ pub fn and<M: Memory>(
 
 pub fn bit<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let m = addr_mode(cpu, AMSelect::V).to_value();
+    let m = addr_mode.dispatch()(cpu, AMSelect::V).to_value();
     let a = cpu.r.a.bitand(m);
     cpu.set_flag(status_flag::N, m & 0x80 != 0);
     cpu.set_flag(status_flag::V, m & 0x40 != 0);
@@ -270,9 +270,9 @@ pub fn bit<M: Memory>(
 
 pub fn eor<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let m = addr_mode(cpu, AMSelect::V).to_value();
+    let m = addr_mode.dispatch()(cpu, AMSelect::V).to_value();
     cpu.r.a = cpu.r.a.bitxor(m);
     cpu.set_flag(status_flag::N, cpu.r.a & 0x80 != 0);
     cpu.set_flag(status_flag::Z, cpu.r.a == 0);
@@ -280,9 +280,9 @@ pub fn eor<M: Memory>(
 
 pub fn ora<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let m = addr_mode(cpu, AMSelect::V).to_value();
+    let m = addr_mode.dispatch()(cpu, AMSelect::V).to_value();
     cpu.r.a = cpu.r.a.bitor(m);
     cpu.set_flag(status_flag::N, cpu.r.a & 0x80 != 0);
     cpu.set_flag(status_flag::Z, cpu.r.a == 0);
@@ -290,9 +290,9 @@ pub fn ora<M: Memory>(
 
 pub fn adc<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let m = addr_mode(cpu, AMSelect::V).to_value();
+    let m = addr_mode.dispatch()(cpu, AMSelect::V).to_value();
     let c = cpu.get_flag(status_flag::C);
     if cpu.get_flag(status_flag::D) == 0 {
         let a = cpu.r.a as u16 + m as u16 + c as u16;
@@ -309,9 +309,9 @@ pub fn adc<M: Memory>(
 
 pub fn cmp<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let m = addr_mode(cpu, AMSelect::V).to_value();
+    let m = addr_mode.dispatch()(cpu, AMSelect::V).to_value();
     let a = cpu.r.a as i16 - m as i16;
     cpu.set_flag(status_flag::N, a & 0x80 != 0);
     cpu.set_flag(status_flag::Z, a == 0);
@@ -320,9 +320,9 @@ pub fn cmp<M: Memory>(
 
 pub fn cpx<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let m = addr_mode(cpu, AMSelect::V).to_value();
+    let m = addr_mode.dispatch()(cpu, AMSelect::V).to_value();
     let a = cpu.r.x as i16 - m as i16;
     cpu.set_flag(status_flag::N, a & 0x80 != 0);
     cpu.set_flag(status_flag::Z, a == 0);
@@ -331,9 +331,9 @@ pub fn cpx<M: Memory>(
 
 pub fn cpy<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let m = addr_mode(cpu, AMSelect::V).to_value();
+    let m = addr_mode.dispatch()(cpu, AMSelect::V).to_value();
     let a = cpu.r.y as i16 - m as i16;
     cpu.set_flag(status_flag::N, a & 0x80 != 0);
     cpu.set_flag(status_flag::Z, a == 0);
@@ -342,9 +342,9 @@ pub fn cpy<M: Memory>(
 
 pub fn sbc<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let m = addr_mode(cpu, AMSelect::V).to_value();
+    let m = addr_mode.dispatch()(cpu, AMSelect::V).to_value();
     let c = cpu.r.sr & 0x1;
     let c = if c == 1 { 0 } else { 1 };
     if cpu.r.sr & status_flag::D == 0 {
@@ -361,9 +361,9 @@ pub fn sbc<M: Memory>(
 
 pub fn dec<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let addr = addr_mode(cpu, AMSelect::A).to_addr();
+    let addr = addr_mode.dispatch()(cpu, AMSelect::A).to_addr();
     let m = cpu.read_byte(addr).wrapping_sub(1);
     cpu.write_byte(addr, m);
     cpu.set_flag(status_flag::N, m & 0x80 != 0);
@@ -372,7 +372,7 @@ pub fn dec<M: Memory>(
 
 pub fn dex<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.r.x = cpu.r.x.wrapping_sub(1);
     cpu.set_flag(status_flag::N, cpu.r.x & 0x80 != 0);
@@ -381,7 +381,7 @@ pub fn dex<M: Memory>(
 
 pub fn dey<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.r.y = cpu.r.y.wrapping_sub(1);
     cpu.set_flag(status_flag::N, cpu.r.y & 0x80 != 0);
@@ -390,9 +390,9 @@ pub fn dey<M: Memory>(
 
 pub fn inc<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let addr = addr_mode(cpu, AMSelect::A).to_addr();
+    let addr = addr_mode.dispatch()(cpu, AMSelect::A).to_addr();
     let m = cpu.read_byte(addr).wrapping_add(1);
     cpu.write_byte(addr, m);
     cpu.set_flag(status_flag::N, m & 0x80 != 0);
@@ -401,7 +401,7 @@ pub fn inc<M: Memory>(
 
 pub fn inx<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.r.x = cpu.r.x.wrapping_add(1);
     cpu.set_flag(status_flag::N, cpu.r.x & 0x80 != 0);
@@ -410,7 +410,7 @@ pub fn inx<M: Memory>(
 
 pub fn iny<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.r.y = cpu.r.y.wrapping_add(1);
     cpu.set_flag(status_flag::N, cpu.r.y & 0x80 != 0);
@@ -419,14 +419,14 @@ pub fn iny<M: Memory>(
 
 pub fn jmp<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    cpu.r.pc = addr_mode(cpu, AMSelect::A).to_addr();
+    cpu.r.pc = addr_mode.dispatch()(cpu, AMSelect::A).to_addr();
 }
 
 pub fn brk<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     if cpu.got_irq {
         return;
@@ -448,7 +448,7 @@ pub fn brk<M: Memory>(
 
 pub fn rti<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     let sr = cpu.pop();
     let pcl = cpu.pop();
@@ -462,19 +462,19 @@ pub fn rti<M: Memory>(
 
 pub fn jsr<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
     let pc = cpu.r.pc + 2;
     cpu.push((pc >> 8) as u8);
     cpu.push((pc & 0xff) as u8);
-    let addr = addr_mode(cpu, AMSelect::A).to_addr();
+    let addr = addr_mode.dispatch()(cpu, AMSelect::A).to_addr();
     cpu.r.pc = addr;
     cpu.count += 6;
 }
 
 pub fn rts<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     let pcl = cpu.pop();
     let pch = cpu.pop();
@@ -485,128 +485,128 @@ pub fn rts<M: Memory>(
 
 pub fn bcc<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
     if cpu.get_flag(status_flag::C) == 0 {
-        cpu.r.pc = addr_mode(cpu, AMSelect::A).to_addr();
-    }
+        cpu.r.pc = addr_mode.dispatch()(cpu, AMSelect::A).to_addr();
+    } else { cpu.r.pc = cpu.r.pc.wrapping_add(addr_mode.length()); }
 }
 
 pub fn bcs<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
     if cpu.get_flag(status_flag::C) == 1 {
-        cpu.r.pc = addr_mode(cpu, AMSelect::A).to_addr();
-    }
+        cpu.r.pc = addr_mode.dispatch()(cpu, AMSelect::A).to_addr();
+    } else { cpu.r.pc = cpu.r.pc.wrapping_add(addr_mode.length()); }
 }
 
 pub fn beq<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
     if cpu.get_flag(status_flag::Z) == 1 {
-        cpu.r.pc = addr_mode(cpu, AMSelect::A).to_addr();
-    }
+        cpu.r.pc = addr_mode.dispatch()(cpu, AMSelect::A).to_addr();
+    } else { cpu.r.pc = cpu.r.pc.wrapping_add(addr_mode.length()); }
 }
 
 pub fn bmi<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
     if cpu.r.sr & status_flag::N != 0 {
-        cpu.r.pc = addr_mode(cpu, AMSelect::A).to_addr();
-    }
+        cpu.r.pc = addr_mode.dispatch()(cpu, AMSelect::A).to_addr();
+    } else { cpu.r.pc = cpu.r.pc.wrapping_add(addr_mode.length()); }
 }
 
 pub fn bne<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
     if cpu.get_flag(status_flag::N) == 1 {
-        cpu.r.pc = addr_mode(cpu, AMSelect::A).to_addr();
-    }
+        cpu.r.pc = addr_mode.dispatch()(cpu, AMSelect::A).to_addr();
+    } else { cpu.r.pc = cpu.r.pc.wrapping_add(addr_mode.length()); }
 }
 
 pub fn bpl<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
     if cpu.get_flag(status_flag::N) == 0 {
-        cpu.r.pc = addr_mode(cpu, AMSelect::A).to_addr();
-    }
+        cpu.r.pc = addr_mode.dispatch()(cpu, AMSelect::A).to_addr();
+    } else { cpu.r.pc = cpu.r.pc.wrapping_add(addr_mode.length()); }
 }
 
 pub fn bvc<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
     if cpu.get_flag(status_flag::V) == 0 {
-        cpu.r.pc = addr_mode(cpu, AMSelect::A).to_addr();
-    }
+        cpu.r.pc = addr_mode.dispatch()(cpu, AMSelect::A).to_addr();
+    } else { cpu.r.pc = cpu.r.pc.wrapping_add(addr_mode.length()); }
 }
 
 pub fn bvs<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
     if cpu.get_flag(status_flag::V) == 1 {
-        cpu.r.pc = addr_mode(cpu, AMSelect::A).to_addr();
-    }
+        cpu.r.pc = addr_mode.dispatch()(cpu, AMSelect::A).to_addr();
+    } else { cpu.r.pc = cpu.r.pc.wrapping_add(addr_mode.length()); }
 }
 
 pub fn clc<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.set_flag(status_flag::C, false);
 }
 
 pub fn cld<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.set_flag(status_flag::D, false);
 }
 
 pub fn cli<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.set_flag(status_flag::I, false);
 }
 
 pub fn clv<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.set_flag(status_flag::V, false);
 }
 
 pub fn sec<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.set_flag(status_flag::C, true);
 }
 
 pub fn sed<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.set_flag(status_flag::D, true);
 }
 
 pub fn sei<M: Memory>(
     cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
     cpu.set_flag(status_flag::I, true);
 }
 
 pub fn nop<M: Memory>(
     _cpu: &mut R6502<M>,
-    _addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    _addr_mode: AM,
 ) {
 }
 
@@ -617,9 +617,9 @@ pub fn nop<M: Memory>(
 // https://www.pagetable.com/c64ref/6502/?tab=2#ISC
 pub fn isc<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let ma = addr_mode(cpu, AMSelect::A).to_addr();
+    let ma = addr_mode.dispatch()(cpu, AMSelect::A).to_addr();
     let m = cpu.read_byte(ma);
     let (m, ov) = m.overflowing_add(1);
     cpu.write_byte(ma, m);
@@ -635,9 +635,9 @@ pub fn isc<M: Memory>(
 // https://www.pagetable.com/c64ref/6502/?tab=2#RRA
 pub fn rra<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let m = addr_mode(cpu, AMSelect::V).to_value();
+    let m = addr_mode.dispatch()(cpu, AMSelect::V).to_value();
     let c = m & 0x1;
     let m = m >> 1;
     let m = (m & 0x7f) | (if cpu.r.sr & status_flag::C != 0 { 0x80 } else { 0 });
@@ -650,9 +650,9 @@ pub fn rra<M: Memory>(
 }
 pub fn lax<M: Memory>(
     cpu: &mut R6502<M>,
-    addr_mode: impl Fn(&mut R6502<M>, AMSelect) -> AMValue,
+    addr_mode: AM,
 ) {
-    let m = addr_mode(cpu, AMSelect::V).to_value();
+    let m = addr_mode.dispatch()(cpu, AMSelect::V).to_value();
     cpu.r.a = m;
     cpu.r.x = m;
     cpu.set_flag(status_flag::N, cpu.r.a & 0x80 != 0);
